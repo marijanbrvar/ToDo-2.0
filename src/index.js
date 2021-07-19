514b7314b679786af40b1960a045338129f5b2ed
Marijan Brvar <marijan.brvar@gmail.com>
2021-07-19 11:00:46 +0200

    Add new class for completed task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 /* eslint-disable no-unused-vars */
 import './style.css';
 import Task from './classes/taks';
+import Completed from './classes/completed';
 
 const todoList = document.querySelector('#todoList');
 
-const listBinding = new Task(todoList);
-listBinding.init();
+// const listBinding = new Task(todoList);
+// listBinding.init();
+
+class App {
+  constructor() {
+    this.task = new Task(todoList);
+    this.completed = new Completed();
+  }
+
+  init() {
+    this.task.init();
+    this.completed.init();
+  }
+}
+
+const app = new App();
+app.init();
