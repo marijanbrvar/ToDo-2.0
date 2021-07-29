ef734e6d64a033eac4a314f5ee76b3eb43f78165
2bleO <Onesis.olea@gmail.com>
2021-07-29 19:08:23 -0400

    Update task.test.js
    Add remove task test

--- src/_test_/task.test.js
+++ src/_test_/task.test.js
@@ -6,17 +6,26 @@ import Task from '../classes/task';
 
 describe('Add and remove task', () => {
   test('Add new task', () => {
-    // const expected = [
-    //   { index: 1, description: "Don't forget Grapes!", completed: false },
-    //   { index: 2, description: "Don't forget Milk!", completed: true },
-    // ];
-
-    const expected = [{ index: 0, description: "Don't forget Cigars!", completed: false }];
     const res = new Task();
     res.addTask("Don't forget Cigars!");
-    // console.log(res.tasks);
 
     expect(res.tasks.length).toBe(1);
+  });
+  test('Check equality', () => {
+    const expected = [
+      { index: 0, description: "Don't forget Cigars!", completed: false },
+      { index: 1, description: "Don't forget Milk!", completed: false },
+    ];
+    const res = new Task();
+    res.addTask("Don't forget Milk!");
+    expect(res.tasks).toStrictEqual(expected);
+  });
+  test('Remove Task', () => {
+    const expected = [
+      { index: 0, description: "Don't forget Cigars!", completed: false },
+    ];
+    const res = new Task();
+    res.deleteTask(1);
     expect(res.tasks).toStrictEqual(expected);
   });
 });
