f5c07ca2f4259630f7ede235e3c218f6f1955572
2bleO <Onesis.olea@gmail.com>
2021-07-30 10:14:12 -0400

    Add editing task and clear all tests

--- src/__test__/task.test.js
+++ src/__test__/task.test.js
@@ -29,3 +29,45 @@ describe('Add and remove task', () => {
     expect(res.tasks).toStrictEqual(expected);
   });
 });
+
+describe('Second partTesting rest of Task functions', () => {
+  test('Function for editing the task description', () => {
+    const expected = [
+      { index: 0, description: "Don't forget Cigars!", completed: false },
+      { index: 1, description: "Don't forget Milk! Edited", completed: false },
+    ];
+    const res = new Task();
+    res.addTask("Don't forget Milk!");
+    res.editTask(1, "Don't forget Milk! Edited");
+    expect(res.tasks).toStrictEqual(expected);
+  });
+  test('Function for updating an item\'s completed status.', () => {
+    const expected = [
+      { index: 0, description: "Don't forget Cigars!", completed: false },
+      { index: 1, description: "Don't forget Milk! Edited", completed: true },
+    ];
+    const res = new Task();
+    res.toggleTask(1);
+    expect(res.tasks).toStrictEqual(expected);
+  });
+  test('Function for updating an item\'s index value upon drag/drop actions.', () => {
+    const expected = [
+      { index: 2, description: "Don't forget Cigars!", completed: false },
+      { index: 1, description: "Don't forget Milk! Edited", completed: true },
+      { index: 0, description: "Don't forget Cigars!", completed: false },
+    ];
+    const res = new Task();
+    res.addTask("Don't forget Cigars!");
+    res.sort(0, 2);
+    expect(res.tasks).toStrictEqual(expected);
+  });
+  test('Clear all completed', () => {
+    const expected = [
+      { index: 2, description: "Don't forget Cigars!", completed: false },
+      { index: 0, description: "Don't forget Cigars!", completed: false },
+    ];
+    const res = new Task();
+    res.clearCompetedTasks();
+    expect(res.tasks).toStrictEqual(expected);
+  });
+});
